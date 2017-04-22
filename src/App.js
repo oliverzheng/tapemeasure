c30d2251b61634be9364a05f1440016719323aaa
Oliver Zheng <gitcommits@oliverzheng.com>
2017-04-21 21:18:47 -0700

    Display device orientation & motion data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
 import React, { Component } from 'react';
+// eslint-disable-next-line
+import FullTilt from 'fulltilt/dist/fulltilt.js'; // needed for gyronorm
+import GyroNorm from 'gyronorm';
+
 import './App.css';
 
 class App extends Component {
+  _gn = new GyroNorm();
+  state = {
+    deviceData: null,
+  };
+
+  componentDidMount() {
+    this._gn.init({
+      frequency: 500,
+    }).then(
+      () => this._gn.start(this._onDeviceUpdate)
+    );
+  }
+
+  componentWillUnmount() {
+    this._gn.stop();
+  }
+
+  _onDeviceUpdate = (data) => {
+    this.setState({
+      deviceData: data,
+    });
+  }
+
   render() {
     return (
       <div className="App">
@@ -11,6 +38,34 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        {this._renderDeviceData()}
+      </div>
+    );
+  }
+
+  _renderDeviceData() {
+    const {deviceData} = this.state;
+    if (!deviceData) {
+      return null;
+    }
+
+    return (
+      <div className="App-deviceData">
+        <div>
+          <p>Device Orientation:</p>
+          <pre>
+            Alpha: {deviceData.do.alpha}<br />
+            Beta : {deviceData.do.beta}<br />
+            Gamma: {deviceData.do.gamma}<br />
+            Absol: {deviceData.do.absolute}<br />
+          </pre>
+          <p>Device Motion:</p>
+          <pre>
+            X: {deviceData.dm.x}<br />
+            Y: {deviceData.dm.y}<br />
+            Z: {deviceData.dm.z}<br />
+          </pre>
+        </div>
       </div>
     );
   }
